1840c9c50d759c56fc9cb7d05e70274e3762a552
yeyesica <yesica.adelia@gmail.com>
2019-11-10 23:42:03 +0700

    react-native-crud

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import Category from './src/Category';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return <Category />;
+  }
+}
